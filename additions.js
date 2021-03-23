abab3e235303d96783965cd879fab775360e634c
Anthony Bradley <26671658+Antagony1060@users.noreply.github.com>
2021-03-23 18:41:02 +0000

    Added POTD 032

--- additions.js
+++ additions.js
@@ -3,7 +3,7 @@
 
 //The last document number for the appropriate set of puzzles
 const lastAotw = 14;
-const lastPotd = 31;
+const lastPotd = 32;
 
 //Get the group-id, to determine which last document number applies
 var lastDocNum;
