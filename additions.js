fd82e411bd642b7b10633d80f777901034fb8c95
Anthony Bradley <26671658+Antagony1060@users.noreply.github.com>
2021-04-09 21:37:40 +0100

    Added POTD 035

--- additions.js
+++ additions.js
@@ -3,7 +3,7 @@
 
 //The last document number for the appropriate set of puzzles
 const lastAotw = 14;
-const lastPotd = 34;
+const lastPotd = 35;
 
 //Get the group-id, to determine which last document number applies
 var lastDocNum;
